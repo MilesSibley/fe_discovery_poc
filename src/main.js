868e7c4f1ed505c3c0a95bb916113b0afeae051d
Miles Sibley <Miles.Sibley@bio-techne.com>
2021-02-24 16:42:07 -0600

    Installed VueSwal

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import router from './router'
 import vuetify from './plugins/vuetify';
 import './assets/formulate.css'
 import VueFormulate from '@braid/vue-formulate'
+import VueSwal from 'vue-swal'
 
 Vue.use(VueFormulate)
+Vue.use(VueSwal)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Scratchpad.vue
+++ src/views/Scratchpad.vue
@@ -1,47 +1,21 @@
 <template>
-    <div class="POC">
-        <h1 class="page-title">Scratchpad</h1>
-        <v-app>
-            <v-container class="grey lighten-5">
-                <v-row>
-                    <v-col v-for="n in 12" :key="n" cols="12" sm="4">
-                        <v-card class="pa-2" outlined tile>
-                            One of three columns
-                            <v-icon large color="green darken-2">
-                                mdi-domain
-                            </v-icon>
-                        </v-card>
-                    </v-col>
-                </v-row>
-            </v-container>
-        </v-app>
-        <Products v-bind:products="products"/>
-    </div>
+  <button @click="showAlert">Hello world</button>
 </template>
 
 <script>
-import axios from "axios";
-import Products from "../components/Products.vue";
+import Swal from 'sweetalert2'
 export default {
-  name: "Scratchpad",
-  components: {
-    Products,
-  },
-  data() {
-    return {
-      products: [],
-    };
-  },
-  created() {
-    axios
-      .get(
-        "https://my-json-server.typicode.com/MilesSibley/JSON-Server/products"
-      )
-      .then((res) => (this.products = res.data))
-      .catch((err) => console.log(err));
+  methods: {
+    showAlert() {
+      // Use sweetalert2
+      Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Your work has been saved',
+        showConfirmButton: false,
+        timer: 1500
+      })
+    },
   },
 };
 </script>
-
-<style scoped>
-</style>
