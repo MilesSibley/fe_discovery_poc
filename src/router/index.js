8d589917f74020bf6556af4c1f1a738bf11801fa
Miles Sibley <Miles.Sibley@bio-techne.com>
2021-02-24 08:31:07 -0600

    Add Scratchpad for testing purposes.

--- src/router/index.js
+++ src/router/index.js
@@ -25,6 +25,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/POC.vue')
+  },
+  {
+    path: '/Scratchpad',
+    name: 'Scratchpad',
+    component: () => import('../views/Scratchpad.vue')
   }
 ]
 
