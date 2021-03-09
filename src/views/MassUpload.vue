892685f31a0b93b955f0a12d483dcc1d025380ae
Miles Sibley <Miles.Sibley@bio-techne.com>
2021-03-09 16:52:03 -0600

    Mass Upload - API calls to local JSON-Server

--- src/views/MassUpload.vue
+++ src/views/MassUpload.vue
@@ -16,14 +16,17 @@
                 :items-per-page="25"
                 class="elevation-1">
                 <template v-slot:item.uploadStatus ="{ item }">
-                    <v-icon class="active status-icon" v-if="item.uploadStatus== 'true'">{{ "mdi-check" }}</v-icon>
+                    <v-icon class="active status-icon" v-if="item.uploadStatus== 'success'">{{ "mdi-check" }}</v-icon>
+                    <v-icon class="inactive status-icon" v-if="item.uploadStatus== 'failure'">{{ "mdi-close-box-outline"}}</v-icon>
                 </template>
             </v-data-table>
+            <FormulateInput type="submit" label="Upload" @click="uploadProducts" />
         </v-app>
     </div>
 </template>
 <script>
     import { VueCsvImport } from 'vue-csv-import';
+    import axios from 'axios';
 
     export default {
         data () {
@@ -58,7 +61,42 @@
                 })       
             },
             updateProductsArray(data){
-                this.products = data
+                //Default the uploadStatus and uploadNotes to empty strings
+                data.map(product => {
+                    product.uploadStatus = ''
+                    product.uploadNotes = ''
+                })
+
+                this.products = data               
+            },
+            uploadProducts(){
+                //For each product, validate the fields, call the API, and update the status
+                this.products.map(product => {
+                    //validate fields
+                    
+                    //Set up API arguements
+                    let payload = {
+                        catnum: product.catnum,
+                        naPricing: product.usd,
+                        euroPricing: product.euro,
+                        gbpPricing: product.gbp,
+                        description: product.longDescription,
+                        productStatus: product.status,
+                    }
+
+                    //call API
+                    axios.post("http://localhost:3000/products",payload)
+                    .then((res) => {
+                        if (res.status == 201) {
+                            product.uploadStatus = 'success'
+                        } else {
+                            product.uploadStatus = 'failure'
+                        }
+                    })
+                    .catch((err) => console.log(err));
+                    //Update status and Notes
+                    //product.uploadStatus = 'success'
+                })
 
             }
         }
