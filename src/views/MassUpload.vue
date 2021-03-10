875fb33dbf92eb9f4a956d24c1454c4bdbd79ce3
Miles Sibley <Miles.Sibley@bio-techne.com>
2021-03-10 10:43:50 -0600

    Mass Upload -  edit fields, validation checks

--- src/views/MassUpload.vue
+++ src/views/MassUpload.vue
@@ -1,26 +1,108 @@
 <template>
     <div>
         <v-app>
-            <FormulateInput
-                type="file"
-                name="file"
-                aligh
-                label="Select your documents to upload"
-                help="Select a .csv file to upload"
-                validation="mime:application/vnd.ms-excel"
-                v-on:file-upload-complete="parseFile"
-            />
-            <v-data-table
-                :headers="headers"
-                :items="products"
-                :items-per-page="25"
-                class="elevation-1">
-                <template v-slot:item.uploadStatus ="{ item }">
-                    <v-icon class="active status-icon" v-if="item.uploadStatus== 'success'">{{ "mdi-check" }}</v-icon>
-                    <v-icon class="inactive status-icon" v-if="item.uploadStatus== 'failure'">{{ "mdi-close-box-outline"}}</v-icon>
-                </template>
-            </v-data-table>
-            <FormulateInput type="submit" label="Upload" @click="uploadProducts" />
+            <v-container>
+                <v-row>
+                    <v-col cols="1"/>
+                    <v-col cols="6">
+                        <h1 >Mass Upload</h1>
+                        
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12">
+                        <FormulateInput
+                            type="file"
+                            name="file"
+                            aligh
+                            label="Select your data file"
+                            help="Select a .csv file to upload"
+                            validation="mime:application/vnd.ms-excel"
+                            v-on:file-upload-complete="parseFile"
+                        />
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12">
+                        <FormulateInput v-if="products.length > 0" type="submit" label="Upload" @click="uploadProducts" />
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12">
+                        <v-data-table
+                            :headers="headers"
+                            :items="products"
+                            :items-per-page="25"
+                            class="elevation-1"
+                        >
+                        
+                            <template v-slot:[`item.catnum`] ="{ item }">
+                                <v-text-field
+                                    ref="catnum"
+                                    v-model="item.catnum"
+                                    :rules = "requiredField"
+                                    
+                                >
+                                </v-text-field>
+                            </template>
+                            <template v-slot:[`item.longDescription`] ="{ item }">
+                                <v-text-field
+                                    ref="longDescription"
+                                    v-model="item.longDescription"
+                                    :rules = "[...requiredField, ...maxLength]"
+                                >
+                                </v-text-field>
+                            </template>
+                            <template v-slot:[`item.status`] ="{ item }">
+                                <v-select
+                                    ref="status"
+                                    v-model="item.status"
+                                    :rules = "requiredField"
+                                    :items="['Active','Discontinued']"
+                                >
+                                </v-select>
+                            </template>
+                            <template v-slot:[`item.usd`] ="{ item }">
+                                <v-text-field
+                                    ref="usd"
+                                    v-model="item.usd"
+                                    :rules = "requiredField"
+                                    prefix="$"
+                                >
+                                </v-text-field>
+                            </template>
+                            <template v-slot:[`item.gbp`] ="{ item }">
+                                <v-text-field
+                                    ref="gbp"
+                                    v-model="item.gbp"
+                                    :rules = "requiredField"
+                                    prefix="$"
+                                >
+                                </v-text-field>
+                            </template>
+                            <template v-slot:[`item.euro`] ="{ item }">
+                                <v-text-field
+                                    ref="euro"
+                                    v-model="item.euro"
+                                    :rules = "requiredField"
+                                    prefix="$"
+                                >
+                                </v-text-field>
+                            </template>
+                            <template v-slot:[`item.uploadStatus`] ="{ item }">
+                                <v-icon ref="uploadStatus" class="active status-icon" v-if="item.uploadStatus == 'success'">{{ "mdi-check" }}</v-icon>
+                                <v-icon class="inactive status-icon" v-if="item.uploadStatus == 'failure'">{{ "mdi-close-box-outline"}}</v-icon>
+                            </template>
+                        </v-data-table>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-spacer></v-spacer>
+                    <v-col cols="6">
+                        <h3 style="color:red">{{APIStatus}}</h3>
+                    </v-col>
+                </v-row>
+            </v-container>
         </v-app>
     </div>
 </template>
@@ -29,8 +111,18 @@
     import axios from 'axios';
 
     export default {
+        created(){
+            axios.get("http://localhost:3000/products")
+                .then((res) => {
+                    if (res.status == 200) {
+                        this.APIStatus = ''
+                    }
+                })
+                .catch((err) => console.log(err));
+        },
         data () {
             return {
+                APIStatus: 'API is Offline',
                 components: { VueCsvImport },
                 headers: [
                     {
@@ -45,7 +137,30 @@
                     { text: 'Upload Status', value: 'uploadStatus' },
                     { text: 'Notes', value: 'uploadNotes' },
                 ],
-                products: [],
+                products: [
+                    // {
+                    //     catnum: 'RDP-343-025',
+                    //     longDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus egestas gravida eros, volutpat pretium arcu accumsan at.',
+                    //     status: 'Active',
+                    //     usd:'100',
+                    //     gbp:'100',
+                    //     euro: 110
+                    // },
+                    // {
+                    //     catnum: 'RDP-ff343-025',
+                    //     longDescription: 'Lorem ipsum.',
+                    //     status: 'Active',
+                    //     usd: '100',
+                    //     gbp: '100',
+                    //     euro: '100'
+                    // }
+                ],
+                requiredField: [
+                    value => !!value || 'Required.',
+                ],
+                maxLength: [
+                    value => (value.length < 100) || 'Max 100 characters',
+                ]
             }
         },
         methods: {
@@ -67,13 +182,12 @@
                     product.uploadNotes = ''
                 })
 
-                this.products = data               
+                this.products = data
+
             },
             uploadProducts(){
                 //For each product, validate the fields, call the API, and update the status
                 this.products.map(product => {
-                    //validate fields
-                    
                     //Set up API arguements
                     let payload = {
                         catnum: product.catnum,
@@ -94,10 +208,11 @@
                         }
                     })
                     .catch((err) => console.log(err));
-                    //Update status and Notes
-                    //product.uploadStatus = 'success'
-                })
 
+                    //Update status and Notes
+                    product.uploadStatus = 'success'
+                    
+                })
             }
         }
     }
